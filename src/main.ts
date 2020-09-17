07f2e6b0f97609f2dd5ac960afb79cc9e8f5ba90
Felix <jonfelixrico@gmail.com>
2020-09-17 21:44:10 +0800

    Set up microservice server code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import 'dotenv-defaults/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+      },
+    }
+  )
+
+  app.listen(() => undefined)
 }
+
 bootstrap()
